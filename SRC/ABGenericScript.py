b139799d58d48791262b2c35723e65fe39d61cb3
Srinathreddy87 <srinathreddy87@gmail.com>
2025-01-09 14:29:01 -0500

    Update ABGenericScript.py

--- SRC/ABGenericScript.py
+++ SRC/ABGenericScript.py
@@ -1,7 +1,7 @@
 """
-This module contains the ABTestDeltaTables class which is used to perform A/B testing
-on Delta tables in Databricks. It includes functionalities for schema comparison
-and row-by-row data validation.
+This module contains the ABTestDeltaTables class which is used to perform
+A/B testing on Delta tables in Databricks. It includes functionalities for
+schema comparison and row-by-row data validation.
 """
 
 import logging
@@ -18,6 +18,7 @@ logging.basicConfig(
     handlers=[logging.StreamHandler()],
 )
 
+
 @dataclass
 class ABTestConfig:
     """
@@ -26,13 +27,17 @@ class ABTestConfig:
     Attributes:
         table_a (str): Name of the first Delta table (A variant).
         table_b (str): Name of the second Delta table (B variant).
-        result_table (str): Name of the result Delta table to store comparison results.
-        key_columns (List[str]): List of key column names used for joining the tables.
+        result_table (str): Name of the result Delta table to store comparison
+                            results.
+        key_columns (List[str]): List of key column names used for joining the
+                                 tables.
     """
+
     table_a: str
     table_b: str
     result_table: str
     key_columns: List[str]
+
 
 class ABTestDeltaTables:
     """
@@ -43,9 +48,10 @@ class ABTestDeltaTables:
         """
         Initialize the A/B test with Delta table names.
 
-        :param config: An instance of ABTestConfig containing the configuration.
+        :param config: An instance of ABTestConfig containing the
+                       configuration.
         """
-        self.spark = SparkSession.builder.getOrCreate()  # Use the existing Spark session in Databricks
+        self.spark = SparkSession.builder.getOrCreate()
         self.table_a = config.table_a
         self.table_b = config.table_b
         self.result_table = config.result_table
@@ -84,7 +90,9 @@ class ABTestDeltaTables:
             df_a[f"{col}_a"] == df_b[f"{col}_b"] for col in self.key_columns
         ]
 
-        joined_df = df_a.join(df_b, join_condition, "outer").select(df_a["*"], df_b["*"])
+        joined_df = df_a.join(df_b, join_condition, "outer").select(
+            df_a["*"], df_b["*"]
+        )
         joined_df.createOrReplaceTempView("joined_view")
 
         # Construct the query for comparison result for each column
@@ -95,7 +103,8 @@ class ABTestDeltaTables:
                 WHEN {renamed_columns_a[col]} = {renamed_columns_b[col]} THEN 'match'
                 ELSE 'unmatch'
             END AS {col.replace('_a', '')}_result
-            """ for col in renamed_columns_a.keys()
+            """
+            for col in renamed_columns_a.keys()
         ]
 
         comparison_query = f"""
@@ -111,30 +120,49 @@ class ABTestDeltaTables:
         """
 
         comparison_df = self.spark.sql(comparison_query)
-        
+
         # Debugging: Show the schema and first few rows of the DataFrame
         comparison_df.printSchema()
         comparison_df.show(10)
 
         # Write the comparison results to the result table
         try:
-            comparison_df.write.format("delta").mode("overwrite").saveAsTable(self.result_table)
+            comparison_df.write.format("delta").mode("overwrite").saveAsTable(
+                self.result_table
+            )
             logger.info(
-                "Data validation complete. Comparison results stored in table: %s", self.result_table
-            )        
-        except (AnalysisException, IllegalArgumentException, StreamingQueryException) as e:
+                "Data validation complete. Comparison results stored in table: %s",
+                self.result_table,
+            )
+        except Exception as e:
             logger.error("Failed to save comparison results: %s", e)
 
+    def _get_table_config(self, table_a: str):
+        """
+        Retrieve the table configuration for the specific table.
 
-if __name__ == "__main__":
-    ab_test_config = ABTestConfig(
-        table_a="table_a",
-        table_b="table_b",
-        result_table="result_table",
-        key_columns=["clm_id"]
-    )
-    ab_test = ABTestDeltaTables(ab_test_config)
+        :param table_a: The name of the table.
+        :return: The config of the table.
+        """
+        domain_ref = dr.Domainreference(self.spark, self.dbutils)
+        table_config = domain_ref.get_table_config(table_a)
+        return table_config
 
-    # Compare schemas and validate data
-    ab_test.compare_schemas()
-    ab_test.validate_data()
+    def table_b_table_path(
+        self, table_a, post_fix, table_config, deployment_mode
+    ):
+        """
+        Determine table_b path for data comparison.
+        """
+        stage_path = table_config["stage_table_path"]
+        if deployment_mode == "feature":
+            table_b = stage_path.replace("_stage", "")
+            return f"{table_a}_{post_fix}"
+
+    def _parse_table_settings(self, table):
+        table_a = table["table"]
+        post_fix = next(
+            (opt["post_fix"] for opt in options if "post_fix" in opt), ""
+        )
+        result_table = table_a + "ab_compresut"
+        return ABTestConfig(table_a, post_fix, result_table)
