d8bdd9c59560ba9124dd517a19313971852215c3
Srinathreddy87 <srinathreddy87@gmail.com>
2025-01-16 17:43:27 -0500

    Update pytest_abgenerictest.py

--- SRC/pytest_abgenerictest.py
+++ SRC/pytest_abgenerictest.py
@@ -1,7 +1,8 @@
 # awscli/SRC/pytest_abgenerictest.py
 
 import pytest
-from unittest.mock import MagicMock
+import logging
+from unittest.mock import MagicMock, patch
 from awscli.SRC.ABGenericScript import ABTestDeltaTables, ABtestconfig
 from awscli.SRC.mocks.mock_spark import MockSparkSession
 
@@ -22,24 +23,29 @@ def ab_compare_fixture():
 def test_get_schema_from_table(ab_compare):
     """Test the get_schema_from_table method."""
     table_name = "test_table"
-    ab_compare.get_schema_from_table = MagicMock(return_value="database.schema")
-    result = ab_compare.get_schema_from_table(table_name)
-    assert result == "database.schema"
+
+    with patch('awscli.SRC.ABGenericScript.Read.format', return_value="database.schema"):
+        result = ab_compare.get_schema_from_table(table_name)
+        assert result == "database.schema"
 
 
-def test_compare_schemas(ab_compare):
+def test_compare_schemas(ab_compare, caplog):
     """Test the compare_schemas method."""
     data_a = [("value1", "value2")]
     schema_a = ["key_column1", "key_column2"]
-    df_a = ab_compare.saprk.createDataframe(data_a, schema= schema_a)
-    ab_compare.spark.registerDataFrameAsTable(df_a, "before_table")
+    df_a = ab_compare.spark.createDataFrame(data_a, schema=schema_a)
+    df_a.createOrReplaceTempView("before_table")
+    
     data_b = [("value1", "value2")]
     schema_b = ["key_column1", "key_column2"]
-    df_b = ab_compare.saprk.createDataframe(data_b, schema= schema_b)
-    ab_compare.spark.registerDataFrameAsTable(df_b, "after_table")
+    df_b = ab_compare.spark.createDataFrame(data_b, schema=schema_b)
+    df_b.createOrReplaceTempView("after_table")
+    
     with caplog.at_level(logging.INFO):
-        ab_compare.compare_schemas("before_table","after_table")
-    assert "Schemas are idnetisclla." in caplog.text
+        ab_compare.compare_schemas("before_table", "after_table")
+    
+    assert "Schemas are identical." in caplog.text
 
 
-
+if __name__ == "__main__":
+    pytest.main()
