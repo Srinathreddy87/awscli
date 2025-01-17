730171d71c2cc1d98dcbb1f8c36bb68ac047adeb
Srinathreddy87 <srinathreddy87@gmail.com>
2025-01-16 23:32:09 -0500

    Update pytest_abgenerictest.py

--- SRC/pytest_abgenerictest.py
+++ SRC/pytest_abgenerictest.py
@@ -115,13 +115,19 @@ def test_validate_data(ab_compare):
 
 
 # Test the create_join_condition method
+# Test the create_join_condition method
 def test_create_join_condition(ab_compare):
-    df1_columns = ["id", "name_id", 123, "age"]
-    df2_columns = ["id", "name_id", 456, "age"]
+    # Mock DataFrames with the 'columns' attribute
+    df1 = MockDataFrame(data=[], schema=["id", "name_id", "age"])
+    df2 = MockDataFrame(data=[], schema=["id", "name_id", "age"])
 
-    join_condition = ab_compare.create_join_condition(df1_columns, df2_columns)
+    join_condition = ab_compare.create_join_condition(df1, df2)
     expected_condition = "df1.id = df2.id AND df1.name_id = df2.name_id"
     assert join_condition == expected_condition
+
+
+if __name__ == "__main__":
+    pytest.main()
 
 
 if __name__ == "__main__":
